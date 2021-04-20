9cb83cabf7a9124e60a91e915791be1253bbf268
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-04-20 09:14:22 -0300

    Segunda aula - flutter

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:dev_quizz/home/level_button/level_button_widget.dart';
 import 'package:dev_quizz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quizz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +14,23 @@ class _State extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                LevelButtonWidget(label: "Médio"),
+                LevelButtonWidget(label: "Difícil"),
+                LevelButtonWidget(label: "Perito"),
+              ],
+            ),
+            QuizCardWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
